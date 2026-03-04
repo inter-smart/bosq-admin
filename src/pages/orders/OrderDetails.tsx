901bd361df87510e9874fb4ade6d4c3dc12d4c1f
Nikhil-Govind-Inter <nikhil@intersmart.in>
2026-03-04 18:49:50 +0530

    added name from address for guest users

--- src/pages/orders/OrderDetails.tsx
+++ src/pages/orders/OrderDetails.tsx
@@ -440,19 +440,16 @@ export default function OrderDetails() {
             </CardTitle>
           </CardHeader>
           <CardContent className="pt-5 space-y-4">
-            <div className="flex items-start gap-3">
+            <div className="flex items-center gap-3">
               <div className="h-9 w-9 rounded-full bg-primary/10 flex items-center justify-center text-primary font-bold">
                 {order.user?.first_name?.[0]}
                 {order.user?.last_name?.[0]}
               </div>
               <div>
                 <p className="font-semibold text-lg leading-none">
-                  {order.user
-                    ? `${order.user.first_name} ${order.user.last_name}`
-                    : "Guest Customer"}
-                </p>
-                <p className="text-sm text-muted-foreground mt-1">
-                  ID: #{order.user?.id || "N/A"}
+                  {order.user.name ? 
+                    order.user.name
+                    : `${order.user.first_name} ${order.user.last_name}`}
                 </p>
               </div>
             </div>
