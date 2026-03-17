276af8a02011c99f2c290644ffd6ac966b6522e9
Nikhil-Govind-Inter <nikhil@intersmart.in>
2026-03-17 14:33:31 +0530

    bug fixed in project images

--- src/schemas/projectImagesSchema.ts
+++ src/schemas/projectImagesSchema.ts
@@ -2,11 +2,10 @@ import { z } from "zod";
 import { commonValidations } from "@/utils/formUtils";
 
 export const projectImagesSchema = z.object({
-  product_variant_id: z.number({
+  project_id: z.number({
     required_error: "Product Variant is required",
     invalid_type_error: "Product Variant must be selected",
   }).min(1, "Product Variant is required"),
-
   media_path: commonValidations.validateFileUpload("Image"),
   media_alt: commonValidations.optionalString("Media Alt Text"),
   media_alt_ar: commonValidations.optionalString("Media Alt Text (Arabic)"),
